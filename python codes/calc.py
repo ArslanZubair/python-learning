fc50c804bf6f6819d43e94e20001df17744bb0da
ArslanZubair <141906252+ArslanZubair@users.noreply.github.com>
2024-08-17 22:40:09 +0500

    first commit

---
+++ python codes/calc.py
@@ -0,0 +1,16 @@
+#calculator
+result:float #variable
+num1= 4
+num2= 2 #declaration & initialization 
+
+
+#add
+result = num1 + num2
+#subtract
+result = num1 - num2
+#multiply
+result = num1 * num2
+#divide
+result = num1 / num2
+
+print(num1 + num2)
---
+++ python codes/helloworld.py
@@ -0,0 +1,22 @@
+ 
+print("Hello world") 
+
+print("my name is arslan zubair")
+
+print(1,2,4)
+
+print("Hello\World")
+
+print("apple","banana","orange")
+
+print("i am ", 21 , "years old")
+
+print("Hello", end=" ")
+print("World")
+
+print('He said, "Hello!"')
+
+print("this number is ", 5)
+
+for i in range(1,6):
+    print(i)
